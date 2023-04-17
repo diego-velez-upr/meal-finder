c3888f6b1ff2e3c23e7e087caa457921074854c9
DiegoLIRodriguez <78493752+DiegoLIRodriguez@users.noreply.github.com>
2023-04-17 15:11:20 -0400

    HTML templates with icons included (#5)
    * HTML templates with icons included
    
    * Fixed listing
    
    - Reformatted code
    - Added missing images
    - Renamed the template.html file to listing.html
    - Fixed loading of CSS and JS
    
    * Wired up pages
    
    - Finished wiring up the map, menu, offers, about, feedback, more and search pages
    - Refactored code
    
    ---------
    
    Co-authored-by: Diego Vélez <diego.velez6@upr.edu>

--- site/listing/urls.py
+++ site/listing/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('listing.html', views.listing, name='listing'),
+    path('offers.html', views.offers, name='offers'),
+    path('about.html', views.about, name='about'),
+    path('feedback.html', views.feedback, name='feedback'),
+    path('map.html', views.map_request, name='map'),
+    path('menu.html', views.menu, name='menu'),
+    path('more.html', views.more, name='more'),
+    path('search.html', views.search, name='search'),
 ]
--- site/listing/views.py
+++ site/listing/views.py
@@ -2,4 +2,35 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'template.html')
+    return render(request, 'index.html')
+
+def listing(request):
+    return render(request, 'listing.html')
+
+
+def offers(request):
+    return render(request, 'offers.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def feedback(request):
+    return render(request, 'feedback.html')
+
+
+def map_request(request):
+    return render(request, 'map.html')
+
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
+def more(request):
+    return render(request, 'more.html')
+
+
+def search(request):
+    return render(request, 'search.html')
